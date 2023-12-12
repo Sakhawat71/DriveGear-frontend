39c70146ac7029ad2ec7136dd3c2e502b92dc41c
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-12 17:58:54 +0600

    create some components and add tailwindCSS & daisyui

---
+++ src/components/pages/Error.jsx
@@ -0,0 +1,13 @@
+
+const Error = () => {
+
+
+    return (
+        <div className="text-center text-red-700">
+            <h1 className="font-bold text-6xl">404</h1>
+            <h3>Page Not Found</h3>
+        </div>
+    );
+};
+
+export default Error;
