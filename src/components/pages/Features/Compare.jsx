b8f0dfffb5f986e144c40ac791c3aff7f6d76b4c
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-21 23:24:17 +0600

    extra sections update -> Compare Car

--- src/components/pages/Features/Compare.jsx
+++ src/components/pages/Features/Compare.jsx
@@ -3,10 +3,10 @@ const Compare = () => {
     return (
         <div className="grid border items-center grid-cols-1 lg:grid-cols-2 max-w-7xl lg:gap-10 my-10 py-10 bg-[#e9e7e7] ">
             
-            <div className="space-y-10 mx-auto items-center justify-center mb-16 lg:mb-0">
+            <div className="space-y-5 text-center lg:text-left px-2 lg:pl-10 md:space-y-10 mx-auto items-center justify-center mb-16 lg:mb-0">
                 <h4 className="text-2xl font-bold text-[#46D993] ">Compare Car</h4>
                 <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">To Compare Your <br /> Favourite Brand Car</h1>
-                <p>Some well-regarded car dealerships known for their customer service,<br /> inventory, and overall reputation.</p>
+                <p className="text-xl">Some well-regarded car dealerships known for their customer service, inventory, and overall reputation.</p>
                 <button className="btn bg-[#46D993]  text-white">Compare Now</button>
             </div>
 
