d8b821c5a64a6921a46714f3bd6d04d424789143
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-24 21:21:02 +0600

    car card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FaStar } from "react-icons/fa";
 
 const ShopCategory = ({ car }) => {
 
@@ -10,16 +11,16 @@ const ShopCategory = ({ car }) => {
             <figure>
                 <img src={image} alt="" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+            <div className="card-body ">
+                <h2 className="card-title text-center">{name}</h2>
 
-                <div className=''>
-                    <p className=''>{brand}</p>
-                    <p>{type}</p>
+                <div className='font-semibold '>
+                    <p className=''>Band: {brand}</p>
+                    <p>Type: {type}</p>
                 </div>
-                <div className='grid grid-cols-2 justify-end'>
-                    <p>Price: {price}</p>
-                    <p>Retting {rating}</p>
+                <div className='grid grid-cols-2 text-xl justify-end'>
+                    <p className='text-left'>Price: {price} $</p>
+                    <p className=''><span className='flex items-center  justify-end'>Retting {rating} <FaStar className='ml-1 text-yellow-400'/></span></p>
                 </div>
                 <div className="card-actions justify-between">
                     <Link to={''} className="btn font-bold text-blue-900">Details</Link>
