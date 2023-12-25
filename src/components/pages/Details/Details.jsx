9f5874dc71394a0968c7e53d961f1b0d03002638
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-25 19:02:33 +0600

    updaate details section

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -1,18 +1,43 @@
-import { useLoaderData,  } from "react-router-dom";
+import { Link, useLoaderData, useParams, } from "react-router-dom";
 
 const Details = () => {
 
-    // const params = useParams();
-    // const id = params.id;
+    const paramsData = useParams()
+    const ProductId = paramsData.id;
     const data = useLoaderData();
-    // console.log(id)
+    const { name, brand, description, price, rating, image } = data;
     console.log(data)
 
-
+    const handelAddToCard = () =>{
+        console.log(ProductId)
+    }
 
     return (
-        <div className="min-h-screen">
-            <h2>{data?.name}</h2>
+        <div className=" my-5 rounded-2xl bg-gray-50 px-5">
+
+            <div className="flex gap-10 py-10 justify-center items-center">
+                <figure className="w-1/2">
+                    <img className="max-h-96" src={image} alt="" />
+                </figure>
+
+                <div className=" w-1/2 p-5">
+                    <h1 className="text-center text-3xl font-bold">{name}</h1>
+                    
+                    <div className="flex justify-around my-5">
+                        <h3 className="text-xl font-bold">Brand: {brand}</h3>
+                        <h3 className="text-xl font-bold">Rating: {rating}☆</h3>
+                    </div>
+
+                    <div className="min-h-[250px] py-5">
+                        <p className="text-gray-600">{description}</p>
+                    </div>
+
+                    <div className="flex justify-around ">
+                        <button className="text-xl font-bold">{price} $</button>
+                        <Link onClick={handelAddToCard} className="text-blue-700 font-bold text-xl">Add to Cart</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
